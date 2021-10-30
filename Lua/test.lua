fb6cd985ace393c640e6057cb6ad627d652f92c8
manyy <manyy.openkt@gmail.com>
2021-10-31 03:28:44 +0800

    modify

--- Lua/test.lua
+++ Lua/test.lua
@@ -1,24 +1,13 @@
-local utils = require "utils"
-
-local function func(arg1, ...)
-  local local1 = 1
-  local local2 = { a = local1 }
-
-  coroutine.yield()
-end
-
-local temp =
+local t =
 {
-  f = (function ()
-    (function ()
-      (function ()
-        func("hello world!", 1, 2, 3)
-      end)()
-    end)()
-  end)
+  a = 1,
+  b = 2,
+  c = 3,
 }
-local co = coroutine.create(temp.f)
-coroutine.resume(co)
 
-utils:dump_obj_print(utils:dump_local(1, co))
-utils:dump_obj_print(utils:dump_local(1), false, false, true)
+local function f ()
+  for i, v in pairs(t) do
+    print(i, v)
+  end
+end
+f()
---
+++ Lua/utils_test.lua
@@ -0,0 +1,24 @@
+local utils = require "utils"
+
+local function func(arg1, ...)
+  local local1 = 1
+  local local2 = { a = local1 }
+
+  coroutine.yield()
+end
+
+local temp =
+{
+  f = (function ()
+    (function ()
+      (function ()
+        func("hello world!", 1, 2, 3)
+      end)()
+    end)()
+  end)
+}
+local co = coroutine.create(temp.f)
+coroutine.resume(co)
+
+utils:dump_obj_print(utils:dump_local(1, co))
+utils:dump_obj_print(utils:dump_local(1), false, false, false)
